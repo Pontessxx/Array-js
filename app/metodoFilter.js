4151fb6ee17e11a7333ca71251af06acb985420b
Pontes <hpo.pontes@gmail.com>
2023-06-15 11:01:55 -0300

    Secao de disponivel apenas ao clicar no disponivel

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -4,6 +4,22 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarDisponibilidade() : filtrarCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
+    if(categoria == 'disponivel'){
+        exibirValorTotalNaTela()
+    }
+}
+function filtrarCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria)
+}
+
+function filtrarDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0)
+}
+
+function exibirValorTotalNaTela(){
+    elementoComValorTotalDisp.innerHTML = `<div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+  </div>`
 }
